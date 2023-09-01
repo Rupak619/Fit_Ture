3fbeadc6fba1cab8b65df655a73e2c62fb3a2fe7
Rupak619 <70577636+Rupak619@users.noreply.github.com>
2023-09-01 14:33:39 +0530

    Add files via upload

---
+++ Fit_Menu/Models/Menu_Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fit_Menu.Models
+{
+    public class Menu_Item
+    {
+        public int Id { get; set; }
+        public string item_name { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        //        public List<string> MenuItems { get; set; }
+    }
+}
